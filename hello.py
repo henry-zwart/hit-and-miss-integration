27fb0eb5e5d09367c1bfee7770cb2c7d95cadb42
TikaBennekum <107108939+TikaBennekum@users.noreply.github.com>
2024-10-30 12:21:20 +0100

    basic mandelbrot func

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from hit-and-miss-integration!")
+# function defining a mandelbrot
+def mandelbrot(x, y, iterations):
+    sample_point = complex(x, y)
 
+    z = 0
+    for i in range(1, iterations):
+        if abs(z) > 2: # a circle with radius 2, goes around mandelbrot set
+            return 0
+        z = z * z + sample_point
+    return 1
 
-if __name__ == "__main__":
-    main()
+print(mandelbrot(0, 0, 1))
