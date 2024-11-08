f58dd12b374657e20f1f0c51b321162d3f1d2c30
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-11-08 14:00:00 +0100

    simplify convergent iteration script

--- scripts/mb_iter_convergence.py
+++ scripts/mb_iter_convergence.py
@@ -6,54 +6,57 @@ from hit_and_mandelbrot import estimate_area, mean_and_ci
 
 def rel_change(i, n_samples, repeats, z=1.96, ddof=1):
     assert i > 0
+    print(f"Testing: i = {i}")
     a1 = estimate_area(n_samples, i - 1, repeats=repeats)
     a2 = estimate_area(n_samples, i, repeats=repeats)
     expected_area, area_ci = mean_and_ci(a2, z=z, ddof=ddof)
     expected_rc, rc_ci = mean_and_ci((a1 - a2) / a1, z=z, ddof=ddof)
-    return expected_rc, rc_ci, expected_area, area_ci
+    return (
+        np.array([expected_rc - rc_ci, expected_rc + rc_ci]),
+        np.array([expected_area - area_ci, expected_area + area_ci]),
+    )
+
+
+def print_rc_ci_msg(ci, threshold):
+    expected_val = ci.mean()
+    ci_radius = ci[1] - expected_val
+    print(f"Relative change: {100*expected_val:.2f}% +- {100*ci_radius:.2f}")
+    if ci[1] < threshold:
+        print(
+            f"Confidence interval below threshold: {100 * expected_val:.2f}% + {100 * ci_radius:.2f}% < {100 * threshold:.2f}%"
+        )
+    else:
+        print(
+            f"Threshold within confidence interval: {100 * expected_val:.2f}% + {100 * ci_radius:.2f}% >= {100 * threshold:.2f}%"
+        )
+    print()
 
 
 def find_pow2_upper_bound(n_samples, threshold, repeats, z=1.96, ddof=1):
     tested_is = []
-    expected_rcs = []
     rc_cis = []
-    expected_areas = []
     area_cis = []
 
     i = 1
-    rc_exp, rc_ci, area_exp, area_ci = rel_change(
-        2**i, n_samples, repeats, z=z, ddof=ddof
-    )
+    rc_ci, area_ci = rel_change(2**i, n_samples, repeats, z=z, ddof=ddof)
     tested_is.append(2**i)
-    expected_rcs.append(rc_exp)
     rc_cis.append(rc_ci)
-    expected_areas.append(area_exp)
     area_cis.append(area_ci)
-    while (rc_exp + rc_ci) > threshold:
-        print(f"{2**i}: {rc_exp*100:.2f}% +- {100*rc_ci:.2f}")
-        print(
-            f"Upper confidence bound exceeds threshold: {100*(rc_exp + rc_ci):.2f}% > {100*threshold}%"
-        )
+    while rc_ci[1] > threshold:
+        print_rc_ci_msg(rc_ci, threshold)
         i += 1
-        rc_exp, rc_ci, area_exp, area_ci = rel_change(
-            2**i, n_samples, repeats, z=z, ddof=ddof
-        )
+        rc_ci, area_ci = rel_change(2**i, n_samples, repeats, z=z, ddof=ddof)
         tested_is.append(2**i)
-        expected_rcs.append(rc_exp)
         rc_cis.append(rc_ci)
-        expected_areas.append(area_exp)
         area_cis.append(area_ci)
 
-    print(f"{2**i}: {rc_exp*100:.2f}% +- {100*rc_ci:.2f}")
-    print(
-        f"Threshold outside confidence interval: {100*(rc_exp + rc_ci):.2f}% <= {100*threshold}%"
-    )
+    print_rc_ci_msg(rc_ci, threshold)
 
-    return tested_is, expected_rcs, rc_cis, expected_areas, area_cis
+    return tested_is, rc_cis, area_cis
 
 
 def minimal_convergence_iteration(n_samples, threshold, repeats, z, ddof):
-    tested_is, expected_rcs, rc_cis, expected_areas, area_cis = find_pow2_upper_bound(
+    tested_is, rc_cis, area_cis = find_pow2_upper_bound(
         n_samples, threshold, repeats, z, ddof
     )
 
@@ -61,26 +64,17 @@ def minimal_convergence_iteration(n_samples, threshold, repeats, z, ddof):
     left, right = tested_is[-2:]
     while left <= right:
         mid = (left + right) // 2
-        print(f"Testing: {mid}")
-        rc_exp, rc_ci, area_exp, area_ci = rel_change(
-            mid, n_samples, repeats, z=z, ddof=ddof
-        )
-        print(f"{mid}: {rc_exp*100:.2f}% +- {100*rc_ci:.2f}")
+        rc_ci, area_ci = rel_change(mid, n_samples, repeats, z=z, ddof=ddof)
+        print_rc_ci_msg(rc_ci, threshold)
+
         tested_is.append(mid)
-        expected_rcs.append(rc_exp)
         rc_cis.append(rc_ci)
-        expected_areas.append(area_exp)
         area_cis.append(area_ci)
-        if (rc_exp + rc_ci) < threshold:
-            print(
-                f"Threshold outside confidence interval: {100*(rc_exp + rc_ci):.2f}% <= {100*threshold}%"
-            )
+
+        if rc_ci[1] < threshold:
             print(f"Lowering upper bound: {right} -> {mid}")
             right = mid - 1
         else:
-            print(
-                f"Upper confidence bound exceeds threshold: {100*(rc_exp + rc_ci):.2f}% > {100*threshold}%"
-            )
             print(f"Raising lower bound: {left} -> {mid}")
             left = mid + 1
         if left >= right:
@@ -89,16 +83,12 @@ def minimal_convergence_iteration(n_samples, threshold, repeats, z, ddof):
 
     sorted_order = np.argsort(tested_is)
     tested_is = np.array(tested_is)[sorted_order]
-    expected_rcs = np.array(expected_rcs)[sorted_order]
     rc_cis = np.array(rc_cis)[sorted_order]
-    expected_areas = np.array(expected_areas)[sorted_order]
     area_cis = np.array(area_cis)[sorted_order]
 
     return (
         tested_is,
-        expected_rcs,
         rc_cis,
-        expected_areas,
         area_cis,
     )
 
@@ -112,20 +102,19 @@ if __name__ == "__main__":
     z = 1.96
     ddof = 1
 
-    tested_is, expected_rcs, rc_cis, expected_area, area_cis = (
-        minimal_convergence_iteration(n_samples, threshold, repeats, z=z, ddof=ddof)
+    tested_is, rc_cis, area_cis = minimal_convergence_iteration(
+        n_samples, threshold, repeats, z=z, ddof=ddof
     )
 
-    min_convergent_i = tested_is[np.argmax(expected_rcs + rc_cis < threshold)]
-    min_convergent_area = expected_area[np.argmax(expected_rcs + rc_cis < threshold)]
+    min_convergent_idx = np.argmax(rc_cis[:, 1] < threshold)
+    min_convergent_i = tested_is[min_convergent_idx]
+    min_convergent_area = area_cis[min_convergent_idx].mean()
 
     fig, axes = plt.subplots(2, sharex=True)
 
     # Scatterplot of the relative change in estimated area, with confidence intervals
-    axes[0].scatter(tested_is, 100 * expected_rcs, s=10, marker="x")
-    axes[0].vlines(
-        tested_is, 100 * (expected_rcs - rc_cis), 100 * (expected_rcs + rc_cis)
-    )
+    axes[0].scatter(tested_is, 100 * rc_cis.mean(axis=1), s=10, marker="x")
+    axes[0].vlines(tested_is, 100 * rc_cis[:, 0], 100 * rc_cis[:, 1])
 
     # Horizontal line at threshold, vertical red line at first i which achieves convergence
     axes[0].axhline(y=100 * threshold, color="grey", linewidth=0.5)
@@ -138,8 +127,8 @@ if __name__ == "__main__":
     axes[0].set_title("Percentage relative change from A(i-1) -> A(i)")
 
     # Scatterplot of the estimated area for each of the tested i's, with confidence intervals
-    axes[1].scatter(tested_is, expected_area, s=10)
-    axes[1].vlines(tested_is, expected_area - area_cis, expected_area + area_cis)
+    axes[1].scatter(tested_is, area_cis.mean(axis=1), s=10)
+    axes[1].vlines(tested_is, area_cis[:, 0], area_cis[:, 1])
 
     # Horizontal red line at minimum convergent area
     axes[1].axhline(min_convergent_area, linestyle="dashed", color="red", linewidth=0.5)
