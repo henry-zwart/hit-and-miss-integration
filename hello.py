9716f1984c15bd509e6100e65ec6568e466bbac2
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-11-08 19:46:34 +0100

    move hello.py

--- hello.py
+++ hello.py
@@ -19,76 +19,22 @@ next steps:
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import qmc
-# from tqdm import trange
+
+from hit_and_mandelbrot.mandelbrot import estimate_area, estimate_area_per_sample
 
 
-def mandelbrot(x, y, iterations):
-    """
-    This function takes a sample point (x, y) and calculates using the nr of
-    iterations wheter it falls within the area of the Mandel brot set or not.
-    If it does, 1 is returned and if not 0 is returned.
-    """
-    c0 = complex(x, y)  # The complex number that is the starting point
+def calculate_area_2(iterations, n_samples, repeats, sampler):
+    area_exp = np.zeros((iterations + 1, n_samples), dtype=np.float64)
+    area_ci = np.zeros_like(area_exp)
 
-    z = 0
-    for _ in range(1, iterations):
-        if abs(z) > 2:  # a circle with radius 2, goes around mandelbrot set
-            return 0
-        z = z * z + c0
-    return 1
-
-
-def random_sampling(n):
-    """Function for the pure random sampling."""
-    return np.reshape(np.random.uniform(-2, 2, n * 2), shape=(n, 2))
-
-
-def hypercube_sampling(n):
-    """Function for the latin hyercube sampling."""
-    sampler = qmc.LatinHypercube(d=2, strength=1)
-    samples = sampler.random(n)
-    samples = (samples - 0.5) * 4  # rescaling from 0 to 1 to -2 to 2
-    return samples
-
-
-def orthognonal_sampling(n):
-    """Function for the orthogonal sampling."""
-    sampler = qmc.LatinHypercube(d=2, strength=2)
-    samples = sampler.random(n)
-    samples = (samples - 0.5) * 4  # rescaling from 0 to 1 to -2 to 2
-    return samples
-
-
-def calculate_proportion(iterations, samples):
-    """
-    Using the generated sample points, this function calculates the
-    proportion of hits to the total amount of sample points.
-    """
-    hits_list = []  # number of points inside mandelbrot set area
-
-    for elem in samples:
-        hits_list.append(mandelbrot(elem[0], elem[1], iterations))
-
-    hits_proportion_list = np.array(hits_list, dtype=float).cumsum() / np.arange(
-        1, len(samples) + 1
-    )
-    return hits_proportion_list
-
-
-def calculate_area(iterations, samples):
-    """
-    Takes the proportional hits from calculate_proportion and uses it to calculate area of
-    Mandelbrot set.
-    It returns a matrix of results from all combinations of number of iterations
-    and number of sample points.
-    """
-    area = np.zeros((iterations + 1, len(samples)))
-    # area_M = calculate_area(iterations, samples)
     for i in range(iterations + 1):
-        area[i] = calculate_proportion(i, samples) * 16
+        exp, ci = estimate_area_per_sample(
+            n_samples, i, repeats=repeats, sampler=sampler
+        )
+        area_exp[i] = exp
+        area_ci[i] = ci
 
-    return area
+    return area_exp, area_ci
 
 
 def balance_i_s(iterations, n):
@@ -96,22 +42,13 @@ def balance_i_s(iterations, n):
     Calculating error for many different amount of sample points, (mean taken over 5).
     Calculating error for many different amount of iterations, (mean taken over 5).
     """
-    nr_of_runs = 5
-    all_sample_convergence = []
-    all_iteration_convergence = []
-    for _ in range(nr_of_runs):
-        print("hey")
-        samples = random_sampling(n)
-        results = calculate_area(iterations, samples)
+    nr_of_runs = 15
 
-        all_iteration_convergence.append(np.abs(results - results[-1]))
-        all_sample_convergence.append(
-            np.abs(results[:, :] - np.expand_dims(results[:, -1], 1))
-        )
+    expected_area, area_ci = calculate_area_2(iterations, n, nr_of_runs, sampler="lhs")
 
     return (
-        np.mean(all_iteration_convergence, axis=0),
-        np.mean(all_sample_convergence, axis=0),
+        np.abs(expected_area - expected_area[-1]),
+        np.abs(expected_area - expected_area[:, -1][:, None]),
     )
 
 
@@ -147,28 +84,29 @@ def plot_area_balanced(iterations, n):
         sample_convergence[-1, 10000:],
     )
 
-    list_errors =  np.arange(0.001, 0.0022, 0.0002)[::-1]
+    list_errors = np.arange(0.0001, 0.0022, 0.00002)[::-1]
+    list_errors = np.arange(0.0001, 0.015, 0.0001)[::-1]
 
     # Finds the number of iterations for certain error values
     for error in list_errors:
-        for i, elem in enumerate(iteration_convergence):
+        for i, elem in enumerate(iteration_convergence, start=20):
             if elem < error:
-                list_iterations.append(i + 1)
+                list_iterations.append(i)
                 break
 
     # Finds the number of sample points for certain error values
     for error in list_errors:
-        for i, elem in enumerate(sample_convergence):
+        for i, elem in enumerate(sample_convergence, start=10000):
             if elem < error:
-                list_samples.append(i + 1)
+                list_samples.append(i)
                 break
 
     list_area = []
 
     # calculates area for the corresponding iterations and sample point amount to the specific errors chosen
     for i in range(len(list_iterations)):
-        samples = random_sampling(list_samples[i])
-        list_area.append(calculate_proportion(list_iterations[i], samples)[-1] * 16)
+        area = estimate_area(list_samples[i], list_iterations[i], repeats=5).mean()
+        list_area.append(area)
 
     # plot
     print("list_area: ", list_area)
@@ -177,6 +115,7 @@ def plot_area_balanced(iterations, n):
     plt.title("Area of Mandelbrot set, with balanced (s, i) combinations taken")
     plt.ylabel("area of Mandelbrot set")
     plt.xlabel("error")
+    print(len(list_area), len(list_errors))
     plt.scatter(list_errors, list_area)
     plt.show()
 
