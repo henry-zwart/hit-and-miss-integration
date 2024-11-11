a9f6d68f0be34391df01a39513e0c3e039f8a701
Henry Zwart <henry.zwart@dragonfly.co.nz>
2024-11-11 21:21:03 +0100

    separate sampling methods onto different rows

--- scripts/plot_sample_convergence.py
+++ scripts/plot_sample_convergence.py
@@ -4,10 +4,11 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 
+from hit_and_mandelbrot.sampling import Sampler
 
 if __name__ == "__main__":
     MIN_SAMPLES = 10
-    ABSOLUTE_THRESHOLD = 1 / 100
+    ABSOLUTE_THRESHOLD = 0.1 / 100
 
     RESULTS_ROOT = Path("data") / "sample_convergence"
     FIGURES_ROOT = Path("figures") / "sample_convergence"
@@ -19,12 +20,16 @@ if __name__ == "__main__":
     with (Path("data") / "shape_convergence" / "metadata.json").open("r") as f:
         target_area = json.load(f)["min_convergent_area"]
 
-    print(target_area)
-    fig, ax = plt.subplots()
-    ax.axhline(target_area, color="grey")
+    fig, axes = plt.subplots(3, sharey=True)
 
-    ax.axhline(target_area * (1 + ABSOLUTE_THRESHOLD), linestyle="dashed", color="grey")
-    ax.axhline(target_area * (1 - ABSOLUTE_THRESHOLD), linestyle="dashed", color="grey")
+    for ax in axes:
+        ax.axhline(target_area, color="grey")
+        ax.axhline(
+            target_area * (1 + ABSOLUTE_THRESHOLD), linestyle="dashed", color="grey"
+        )
+        ax.axhline(
+            target_area * (1 - ABSOLUTE_THRESHOLD), linestyle="dashed", color="grey"
+        )
 
     # For each sampling method, plot the lower and upper confidence interval, at each sample size
     colours = {
@@ -32,32 +37,24 @@ if __name__ == "__main__":
         "lhs": "orange",
         "ortho": "green",
     }
-    for sampler in ("random", "lhs"):
+    for i, sampler in enumerate(Sampler):
         area = np.load(RESULTS_ROOT / f"{sampler}_area.npy")[2000:]
         ci = np.load(RESULTS_ROOT / f"{sampler}_ci.npy")[2000:]
         sample_size = np.load(RESULTS_ROOT / f"{sampler}_sample_size.npy")[2000:]
         lower = area - ci
         upper = area + ci
-        # ax.plot(
-        #     sample_size,
-        #     lower,
-        #     label=sampler,
-        #     color=colours[sampler],
-        #     linewidth=1,
-        # )
-        # ax.plot(
-        #     sample_size,
-        #     upper,
-        #     color=colours[sampler],
-        #     linewidth=1,
-        # )
-        ax.fill_between(
+        axes[i].fill_between(
             sample_size, lower, upper, color=colours[sampler], alpha=0.5, label=sampler
         )
-        ax.plot(
+        axes[i].plot(
             sample_size, area, color=colours[sampler], linestyle="dashed", linewidth=1
         )
 
     fig.legend()
     fig.tight_layout()
     fig.savefig(FIGURES_ROOT / "area.png", dpi=700)
+
+    # Plot distribution over samples-till-convergence.
+    # i.e. for each repeat, on each sampling method, record the number of sampling such
+    #       that the confidence intervals afterward are totally within the bounds.
+    #       Plot this as a histogram or KDE.
